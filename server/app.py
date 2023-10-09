baf178a115215951a6496b8d0596a67aec33e9b5
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-10-09 16:12:07 -0400

    Lab completed

--- server/app.py
+++ server/app.py
@@ -1,33 +1,132 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, jsonify, make_response
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
-
 from config import app, db, api
 from models import User, Recipe
 
+
 class Signup(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        username = json.get("username")
+        password = json.get("password")
+        image_url = json.get("image_url")
+        bio = json.get("bio")
+
+        user = User(
+            username=username,
+            password_hash=password,
+            image_url=image_url,
+            bio=bio,
+        )
+
+        if user.validate_username(username, user.username):
+            db.session.add(user)
+            db.session.commit()
+
+            session["user_id"] = user.id
+
+            response = make_response(
+                user.to_dict(),
+                201,
+            )
+
+            return response
+        else:
+            return {}, 422
+
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session["user_id"]:
+            user = User.query.get(session["user_id"])
+
+            return make_response(
+                user.to_dict(),
+                200,
+            )
+        else:
+            return {}, 401
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+
+        username = json.get("username")
+        password = json.get("password")
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.authenticate(password):
+            session["user_id"] = user.id
+            return user.to_dict(), 201
+        else:
+            return {}, 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if session["user_id"]:
+            session["user_id"] = None
+            return {}, 204
+        else:
+            return {}, 401
+
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if session["user_id"]:
+            return [
+                recipe.to_dict()
+                for recipe in Recipe.query.filter_by(user_id=session["user_id"]).all()
+            ], 200
+        else:
+            return {}, 401
 
-api.add_resource(Signup, '/signup', endpoint='signup')
-api.add_resource(CheckSession, '/check_session', endpoint='check_session')
-api.add_resource(Login, '/login', endpoint='login')
-api.add_resource(Logout, '/logout', endpoint='logout')
-api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
+    def post(self):
+        if not session.get("user_id"):
+            return {"error": "Unauthorized"}, 401
+
+        json_data = request.get_json()
+
+        # Validate title
+        title = Recipe.validate_title(None, "title", json_data.get("title"))
+        if title is None:
+            return {"error": "Title is required"}, 422
+
+        # Validate instructions
+        instructions = Recipe.validate_instructions(
+            None, "instructions", json_data.get("instructions")
+        )
+        if instructions is None:
+            return {"error": "Instructions must be at least 50 characters long."}, 422
+
+        minutes_to_complete = json_data["minutes_to_complete"]
+
+        # Create and save the recipe
+        user_id = session["user_id"]
+        recipe = Recipe(
+            title=title,
+            instructions=instructions,
+            minutes_to_complete=minutes_to_complete,
+            user_id=user_id,
+        )
+
+        db.session.add(recipe)
+        db.session.commit()
+
+        return recipe.to_dict(), 201
 
 
-if __name__ == '__main__':
+api.add_resource(Signup, "/signup", endpoint="signup")
+api.add_resource(CheckSession, "/check_session", endpoint="check_session")
+api.add_resource(Login, "/login", endpoint="login")
+api.add_resource(Logout, "/logout", endpoint="logout")
+api.add_resource(RecipeIndex, "/recipes", endpoint="recipes")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,62 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
-
+from sqlalchemy.orm import validates
 from config import db, bcrypt
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.exc import IntegrityError
+
 
 class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
 
-    pass
+    recipes = db.relationship("Recipe", backref="user", lazy=True)
+
+    serialize_rules = ("-recipes",)
+
+    def __repr__(self):
+        return f"User {self.username}, ID: {self.id}"
+
+    @property
+    def password_hash(self):
+        raise AttributeError("Password is not accessible")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode("utf-8"))
+
+    @validates("username")
+    def validate_username(self, key, value):
+        if value == "":
+            raise ValueError("Username is required")
+        return value
+
 
 class Recipe(db.Model, SerializerMixin):
-    __tablename__ = 'recipes'
-    
-    pass
+    __tablename__ = "recipes"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    @validates("title")
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Title is required")
+        else:
+            return title
+
+    @validates("instruction")
+    def validate_instructions(self, key, instructions):
+        if len(instructions) <= 50:
+            return None
+        else:
+            return instructions
